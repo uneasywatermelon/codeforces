aceedcff1865e95d9708df7aaf2b63c2498b5f81
uneasywatermelon <marioalemany16@gmail.com>
2025-10-22 19:24:39 -0700

    semicolons for python

--- 2154A.py
+++ 2154A.py
@@ -1,28 +1,28 @@
 for _ in range(int(input())):
-    n, k = map(int,input().split())
-    s = input()
+    n, k = map(int,input().split());
+    s = input();
     
-    distances = []
-    first1 = None
+    distances = [];
+    first1 = None;
     for x in range(len(s)):
         if s[x]=='1':
-            first1 = x
-            break
-    sofar = 0
+            first1 = x;
+            break;
+    sofar = 0;
     if first1 != None:
         for x in range(first1 + 1, len(s)):
             if s[x] == '1':
-                distances.append(sofar)
-                sofar = 0
+                distances.append(sofar);
+                sofar = 0;
             else:
-                sofar += 1
+                sofar += 1;
 
 
-    cnt = 0
+    cnt = 0;
     if distances:
         for x in distances:
-            cnt += 1 if x >= (k-1) else 0
+            cnt += 1 if x >= (k-1) else 0;
     if first1 != None:
-        cnt += 1
+        cnt += 1;
 
-    print(cnt)
+    print(cnt);
